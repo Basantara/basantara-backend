4a59a5cf163683cfad594331d32fb30fac5caf29
RovinoAlana <123210086@student.upnyk.ac.id>
2024-06-16 21:04:59 +0700

    documentation edit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,21 @@
-const { Router } = require('express');
+const express = require('express');
 const alphabetsRoutes = require('./alphabetsRoutes.js');
 const usersRoutes = require('./usersRoutes.js');
 const { authenticateToken } = require('../middleware/authentication.js');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./documentation.yaml');
 
-const router = Router();
+const app = express();
 
-router.use('/api/alphabets', authenticateToken, alphabetsRoutes);
-router.use('/api/users', usersRoutes);
+app.use(express.json());
+app.use('/api/alphabets', authenticateToken, alphabetsRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-module.exports = router;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
